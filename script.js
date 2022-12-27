ad4e7e1106fbddd7e23fc8f62ce77a6bed036e01
Kartik Chindarkar <kchindarkar@deloitte.com>
2022-12-27 14:26:07 +0530

    final js commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+function hide() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function open() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (const element of btnsOpenModal) {
+  element.addEventListener('click', open);
+
+  btnCloseModal.addEventListener('click', hide);
+
+  overlay.addEventListener('click', hide);
+}
